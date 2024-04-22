149b091d50ebecab8e72f575747e18fa0fd5973b
yup50 <147809042+yup50@users.noreply.github.com>
2024-04-22 15:16:47 +0200

    de 4 kleuren staan er

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Bling Bling Nail Studio Chantal</h2>
+
+    <input type="color" value="#CD00FF"></input>
+    <input type="color" value="#FF0000"></input>
+    <input type="color" value="#F0FF00"></input>
+    <input type="color" value="#2AFF00"></input>
+
+
+</body>
+</html>
